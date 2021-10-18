b26240b707319ae71d7e4b07fa8486b0dcf4fea1
a11155 <55248821+a11155@users.noreply.github.com>
2021-10-18 22:53:16 +0300

    Ніби має работать

---
+++ Model/main.py
@@ -0,0 +1,26 @@
+from Util.MapController import Map
+from CarController import CarDriver
+
+
+def main():
+    Map.init("test.txt")
+
+    CarDriver.init()
+    for node in CarDriver.cars_array[0].way:
+        print(node)
+
+    print()
+    print()
+    print("Simulation: ")
+    print()
+    print()
+    for i in range(20):
+
+        for car in CarDriver.cars_array:
+            print(car.x)
+            print(car.getLines()[car.currentLine])
+        CarDriver.comp()
+        print()
+        print()
+if __name__ == "__main__":
+    main()
