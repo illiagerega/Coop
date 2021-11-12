79d33adbd35ca9aa5af5020c90a24d9e3f3cab95
Illia Gerega <ahafamohamed@gmail.com>
2021-11-12 19:57:22 +0200

    advanced 3d

--- Front/static/js/main.js
+++ Front/static/js/main.js
@@ -1,12 +1,30 @@
 const main = new function(){
     const main = this;
 
+    const MOUSE_BUTTON = ['left', 'middle', 'right'];
+
     let scene, renderer, camera,
+    config = {
+        'mouse_lock' : false
+    },
     keys = {}, 
+    mouse = {
+        'position' : {'x' : 0, 'y' : 0},
+        'speed' : {'x' : 0, 'y' : 0},
+        'locked' : false,
+        'keys' : {
+            'left' : false,
+            'right' : false,
+            'middle' : false,
+            'wheel' : 0
+        }
+    },
     events = {
         'keydown' : null,
         'keypress' : null,
         'keyup' : null,
+        'mousedown' : null,
+        'mouseup' : null
     };
 
     const v = this.v = (x, y, z) => {
@@ -43,23 +61,70 @@ const main = new function(){
         requestAnimationFrame(animate);
     };
 
-    this.init = () => {
+    this.init = (settings) => {
 		scene = new THREE.Scene();
 		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize(window.innerWidth, window.innerHeight);
 
-		window.addEventListener('keydown', (e) => {
-			if (!keys[e.code]) {
-				keys[e.code] = true;
-				call_event('keypress', keys);
-			}
-		});
+        if (settings.keys_capture){
+            window.addEventListener('keydown', (e) => {
+                e.preventDefault();
+                if (!keys[e.code]) {
+                    keys[e.code] = true;
+                    call_event('keypress', keys);
+                }
+            });
+    
+            window.addEventListener('keyup', (e) => {
+                e.preventDefault();
+                keys[e.code] = false;
+                call_event('keyup', keys);
+            });
+        }
 
-		window.addEventListener('keyup', (e) => {
-			keys[e.code] = false;
-			call_event('keyup', keys);
-		});
+        if (settings.mouse_capture){
+
+            if(settings.mouse_capture){
+                if(settings.mouse_lock){
+                    config.mouse_lock = true;
+                    document.addEventListener('pointerlockchange', (e) => {
+                        mouse.locked = !!document.pointerLockElement
+                    })
+                }
+            }
+
+            window.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
+            });
+
+            window.addEventListener('mousedown', (e) => {
+                e.preventDefault();
+                mouse.keys[MOUSE_BUTTON[e.button]] = true;
+                call_event('mousedown', mouse);
+
+                if(config.mouse_lock && !mouse.locked){
+                    console.log('locked')
+                    document.body.requestPointerLock();
+                }
+            });
+
+            window.addEventListener('mouseup', (e) => {
+                e.preventDefault();
+                mouse.keys[MOUSE_BUTTON[e.button]] = false;
+                call_event('mouseup', mouse);
+            });
+
+            window.addEventListener('mousemove', (e) => {
+                e.preventDefault();
+                mouse.position.x = e.screenX;
+                mouse.position.y = e.screenY;
+                mouse.speed.x = e.movementX;
+                mouse.speed.y = e.movementY;
+                call_event('mousemove', mouse);
+            });
+        }
+		
 
 		document.body.appendChild(renderer.domElement);
 		animate();
