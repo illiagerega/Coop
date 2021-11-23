d990eeaa74f3d98b168da715d99673ca6b2efda2
LolzI <70146541+LoolzMe@users.noreply.github.com>
2021-11-23 21:42:45 +0200

    added zoom

--- Front/static/js/js.js
+++ Front/static/js/js.js
@@ -1,64 +1,5 @@
-var up = false,
-    right = false,
-    down = false,
-    left = false,
-    x = window.innerWidth/2-130/2,
-    y = window.innerHeight/2-130/2
-document.addEventListener('keydown',press)
-function press(e){
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
-    up = true
-  }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
-    right = true
-  }
-  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
-    down = true
-  }
-  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
-    left = true
-  }
-}
-document.addEventListener('keyup',release)
-function release(e){
-  if (e.keyCode === 38 /* up */ || e.keyCode === 87 /* w */ || e.keyCode === 90 /* z */){
-    up = false
-  }
-  if (e.keyCode === 39 /* right */ || e.keyCode === 68 /* d */){
-    right = false
-  }
-  if (e.keyCode === 40 /* down */ || e.keyCode === 83 /* s */){
-    down = false
-  }
-  if (e.keyCode === 37 /* left */ || e.keyCode === 65 /* a */ || e.keyCode === 81 /* q */){
-    left = false
-  }
-}
-function gameLoop(){
-  var div = document.querySelector('div')
-  if (up){
-    y = y - 10
-  }
-  if (right){
-    x = x + 10
-  }
-  if (down){
-    y = y + 10
-  }
-  if (left){
-    x = x - 10
-  }
-  div.style.left = x+'px'
-  div.style.top = y+'px'
-  window.requestAnimationFrame(gameLoop)
-}
-window.requestAnimationFrame(gameLoop)
-
-var map_ajax_state_sending = false;
-
-
 // AJAX receivement
-
+var map_ajax_state_sending = false;
 
 
 function setCars() {
@@ -161,10 +102,11 @@ var up = false,
 var mousePosX = 0, mousePosY = 0;
 
 var offset = 10; // default: 10 px per frame
-var zoom_scale = -0.002;
+var zoom_scale = -0.001;
 var zoom_max = 4;
 var zoom_min = 0.125;
 var zoom_delta = 0;
+var scale = 1;
 
 document.addEventListener('keydown', press);
 function press(e){
@@ -215,21 +157,36 @@ function zoom(e){
     zoom_delta = e.deltaY;
     //wheel = true;
 
-    scale = zoom_delta * zoom_scale;
+    var new_scale = scale + zoom_delta * zoom_scale;
 
     // restriction of scaling
-    scale = Math.min(Math.max(zoom_min, scale), zoom_max);
+    new_scale = Math.min(Math.max(zoom_min, new_scale), zoom_max);
 
-    //element.css("transform-origin", (x + mousePosX).toString() + "px " + (mousePosY + y).toString() + "px")
-    $("#movable_scene").css("transform", "scale(" + scale + ")");
+    var element = $("#movable_scene");
+    var position = element.position();
+    const x = position.left, y = position.top;
+    const parentRect = document.getElementById("movable_scene").parentNode.getBoundingClientRect();
+    // element.css("transform-origin", (mousePosX * scale - x * scale).toString() + "px " + (mousePosY * scale - y * scale).toString() + "px")
+    // element.css("left", 0);
+    // element.css("top", 0);
+    // (mousePosX - x) * (scale - 1)
+    var xPercent = ((mousePosX - x)).toFixed(2);
+    var yPercent = ((mousePosY - y)).toFixed(2);
+    var left = Math.round(mousePosX - parentRect.left - (xPercent * (new_scale / scale)));
+    var top = Math.round(mousePosY - parentRect.top - (yPercent * (new_scale / scale)));
+  
+    element.css("transform", "scale(" + new_scale + ")");
+    element.css("left", left);
+    element.css("top", top);
 
+    scale = new_scale;
 }
 
 document.onmousemove = getCursorXY;
 function getCursorXY(e){
     mousePosX = (window.Event) ? e.pageX : e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
     mousePosY = (window.Event) ? e.pageY : e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-}
+  }
 
 // main movement function
 
@@ -267,20 +224,20 @@ window.requestAnimationFrame(mainMovement)
 
 // setInterval(mode, 2000);
 
-var notepad = document.getElementById("car_red");
-notepad.addEventListener("contextmenu",function(event){
-    event.preventDefault();
-    var ctxMenu = document.getElementById("ctxMenu");
-    ctxMenu.style.display = "block";
-    ctxMenu.style.left = (event.pageX - 10)+"px";
-    ctxMenu.style.top = (event.pageY - 10)+"px";
-},false);
-notepad.addEventListener("click",function(event){
-    var ctxMenu = document.getElementById("ctxMenu");
-    ctxMenu.style.display = "";
-    ctxMenu.style.left = "";
-    ctxMenu.style.top = "";
-},false);
+// var notepad = document.getElementById("car_red");
+// notepad.addEventListener("contextmenu",function(event){
+//     event.preventDefault();
+//     var ctxMenu = document.getElementById("ctxMenu");
+//     ctxMenu.style.display = "block";
+//     ctxMenu.style.left = (event.pageX - 10)+"px";
+//     ctxMenu.style.top = (event.pageY - 10)+"px";
+// },false);
+// notepad.addEventListener("click",function(event){
+//     var ctxMenu = document.getElementById("ctxMenu");
+//     ctxMenu.style.display = "";
+//     ctxMenu.style.left = "";
+//     ctxMenu.style.top = "";
+// },false);
 
 // var notepad = document.getElementById("car_red");
 // notepad.addEventListener("contextmenu",function(event){
