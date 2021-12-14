4c0a32d0ba2ed47aa80528d30970dec2e2d593f3
LolzI <70146541+LoolzMe@users.noreply.github.com>
2021-12-14 21:39:50 +0200

    Probably fixed unknown bug

---
+++ Back/Model/Util/AdvancedAlgorithms.py
@@ -0,0 +1,19 @@
+from start import info
+from .Consts import *
+from .MapController import Map
+import math
+
+class AdvencedGraphAlgorithms:
+
+    @staticmethod
+    def DFSRoads(roads, start_node, roads_colored):
+        path = []
+        lenght = 0
+        max_velocity = MaxVelocity
+        n_lines = math.inf
+
+        
+
+
+class OptimizedGraph:
+    pass
