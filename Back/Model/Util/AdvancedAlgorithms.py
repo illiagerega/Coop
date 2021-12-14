c63b957fad8b81a837203fa32905fcdc8ab566c4
LolzI <70146541+LoolzMe@users.noreply.github.com>
2021-12-14 23:24:33 +0200

    Update

--- Back/Model/Util/AdvancedAlgorithms.py
+++ Back/Model/Util/AdvancedAlgorithms.py
@@ -2,9 +2,14 @@ from start import info
 from .Consts import *
 from .NodeInstance import Node
 from .RoadInstance import Road
+from .TrafficLightInstance import TrafficLight
 import math
 
-class AdvencedGraphAlgorithms:
+# The main module for advanced algorithms which created for solving the 6th task.
+# It includes DFSRoads for optimizing (vertexes and edges compressing) a traffic system
+# Of course, It includes GA algorithm 
+
+class AdvancedGraphAlgorithms:
 
     @staticmethod
     def DFSRoads(roads, start_node, roads_colored):
@@ -14,7 +19,26 @@ class AdvencedGraphAlgorithms:
         n_lines = math.inf
 
         
-        
+class OptimizedNode:
+    def __init__(self, node: Node, traffic_light: TrafficLight = None):
+        self.index = node.index
+        self.adj_roads = node.adj_nodes
+        self._traffic_light = traffic_light
+
+
+class OptimizedRoad:
+    def __init__(self, length: int, max_velocity: int, n_lines: int, start_node: int, end_node: int):
+        self.length = length
+        self.max_velocity = max_velocity
+        self.n_lines = n_lines
+        self.start_node = start_node
+        self.end_node = end_node
+
+    def getFlow(self):
+        pass
+
+    def getDensity(self):
+        pass
 
 
 class OptimizedGraph:
